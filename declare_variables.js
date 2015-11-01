b0f44abc46fdb77857c8b27cb2868c0c9a0c12c4
Hepic <hepic_@hotmail.com>
2015-11-01 10:18:48 +0200

    starting files

---
+++ declare_variables.js
@@ -0,0 +1,5 @@
+var canvas = document.getElementById('cnv');
+var ctx = canvas.getContext('2d');
+var W = canvas.width, H = canvas.height;
+
+
---
+++ render.js
@@ -0,0 +1,16 @@
+function draw_background()
+{
+    ctx.beginPath();
+    ctx.rect(0, 0, W, H);
+    ctx.fillStyle = 'cyan';
+    ctx.fill();
+}
+
+
+(function render()
+{
+    ctx.clearRect(0, 0, W, H);   
+    draw_background();
+})();
+
+
